8d858d5011e6a31779a7006524bf121ef5427f73
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-01-30 14:04:45 +0800

    Barebones Login form
    Will apply CSS later.

--- frontend/src/components/component_list.jsx
+++ frontend/src/components/component_list.jsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react";
+import '../styles/login.css';
 
 const Login = () => {
     const [Email, setEmail] = useState('');
@@ -6,19 +7,39 @@ const Login = () => {
 
     const out = (e) => {
         e.preventDefault();
-        console.log(`Onclicking, We obtained that your email is ${Email} and password is ${Pass} during input`);
+        let emptyFields = []
+        if(!Email){
+          emptyFields.push('Email')
+        }
+        if(!Pass){
+          emptyFields.push('Pass')
+        }
+        if(emptyFields.length > 0){
+          console.log('Please fill in all the fields', emptyFields);
+        } else {
+          console.log(`Onclicking, We obtained that your email is ${Email} and password is ${Pass} during input`);
+        }
     }
 
     return (
         <>
-        <form>
-            <input type="email" value={Email} 
-            placeholder="Your Email"
-            onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" value={Pass}
-            placeholder="Your Password"
-            onChange={(e) => setPass(e.target.value)}/>
-            <button onClick={out}>Submit Credentials</button>
+        <header><h1>This is header for icon/hero icon</h1></header>
+
+        <form className="logcontainer">
+          <h1>Sign in to get started</h1>
+
+          <label htmlFor="Email">Email:</label>
+          <input type="email" value={Email} 
+          placeholder="Your Email" className="logemail"
+          onChange={(e) => setEmail(e.target.value)}/>
+          <label htmlFor="Pass">Password:</label>
+          <input type="password" value={Pass}
+          placeholder="Your Password" className="logpass"
+          onChange={(e) => setPass(e.target.value)}/>
+
+          <input type="submit" value="Login" onClick={out}></input>
+          <button>Register</button>
+          <a href="/#">Forgot Password?</a>
         </form>
         </>
     )
