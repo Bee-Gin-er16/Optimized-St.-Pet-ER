f987f1c941f181fabfadc690aba5d8d38473fa0a
Bee-Gin-er16 <hanseubertdelacruz@gmail.com>
2024-03-25 22:34:03 +0800

    Put some default values for profile
    The pet section might not workout as well as I hoped. Maybe put all work into the profile as a profile itself.

--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -1,32 +1,53 @@
 import prof_style from '../styles/profile.module.css';
 import default_profile from "../img/pet_and_owner.jpg";
+import default_pet from "../img/f'luffy.jpg";
 
 
-const Profile = () => {
+const Profile = (props) => {
   return (
     <>
-    <div className={prof_style.profile_contents}>
+    
+    <div className={`${prof_style.profile_contents}`}>
 
-      <div className={prof_style.user_profile}>
-        <div className={prof_style.profile_bg}>
-          <div className={prof_style.profile_img}>
-            <img src={default_profile} alt="user_profile_picture"/> 
+      {/*User Profile*/}
+      <div className={`${prof_style.user_profile} `}>
+        <div className={`${prof_style.profile_bg} `}>
+          <div className={`${prof_style.profile_img}`}>
+            <img src={props.user_pic} alt="user_profile_picture"/> 
           </div>
-          <div className={prof_style.profile_cred}>
-            <h1 className={prof_style.h1}>ADAM BLACK</h1>
-            <h2 className={prof_style.h2} >Address:</h2>
-            <h2 className={prof_style.h2} >Status:</h2>
-            <h2 className={prof_style.h2} >Gender:</h2>
+          <div className={`${prof_style.profile_cred}`}>
+            <h2 className={`${prof_style.h2} `}>Name: {props.user_name}</h2>
+            <h3 className={`${prof_style.h3} `}>Address: {props.user_add}</h3>
+            <h3 className={`${prof_style.h3} `}>Status: {props.user_stat}</h3>
+            <h3 className={`${prof_style.h3} `}>Gender: {props.user_gender}</h3>
+            <h3 className={`${prof_style.h3} `}>Email: {props.user_email}</h3>
+            <h3 className={`${prof_style.h3} `}>Mobile: {props.user_mobile}</h3>
           </div>
         </div>
       </div>
 
-      <div className={prof_style.pet_profile}>
+      {/*Pet Profile: Will need to be dynamic in order to switch info per pet owned*/}
+      <div className={`${prof_style.pet_profile} `}>
+        <img src={props.pet_pic} alt="pet_picture" />
         <h1>This is pet profile</h1>
       </div>
     </div>
     </>
   );
+}
+
+Profile.defaultProps = {
+  //User defaults
+  user_pic: default_profile,
+  user_name: "GUEST NAME",
+  user_add: "Cebu City",
+  user_stat: "Administrator",
+  user_gender: "Male",
+  user_email: "hello@gmail.com",
+  user_mobile: "01234234534",
+
+  //Pet Profile defaults
+  pet_pic: default_pet
 }
 
 export {Profile}
